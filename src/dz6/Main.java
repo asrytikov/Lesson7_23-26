909011796cdf37d0814a88d3feb0203d8fa5cdd7
asrytikov <asrytikov@gmail.com>
2023-10-27 21:02:09 +0400

    Initial commit

---
+++ src/dz6/Main.java
@@ -0,0 +1,24 @@
+/*Д/з Класс Phone.
+        Создайте класс Phone, который содержит переменные number, model и weight.
+        Создайте три экземпляра этого класса.
+        Выведите на консоль значения их переменных.
+        Добавить в класс Phone методы: receiveCall, имеет один параметр – имя звонящего. Выводит на консоль сообщение “Звонит {name}”.
+        Метод getNumber – возвращает номер телефона. Вызвать эти методы для каждого из объектов.
+        Добавить конструктор в класс Phone, который принимает на вход три параметра для инициализации переменных класса - number, model и weight.
+        Добавить конструктор, который принимает на вход два параметра для инициализации переменных класса - number, model.
+        Добавить конструктор без параметров.
+
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Phone phone = new Phone();
+        phone.setModel("Samsung S55");
+        phone.setNumber("999-999-999");
+        phone.setWeight(100.123);
+
+        phone.receiveCall("Misha");
+        phone.info();
+        System.out.println(phone.getNumber());
+    }
+}
---
+++ src/dz6/Phone.java
@@ -0,0 +1,54 @@
+public class Phone {
+
+   private String number;
+   private String model;
+   private double weight;
+
+    public Phone() {
+        System.out.println("Phone is created");
+    }
+
+    public Phone(String number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+    public Phone(String number, String model, double weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void receiveCall(String name){
+        System.out.printf("Call %s\n", name);
+    }
+
+    public void info(){
+        System.out.printf("Phone: model=%s, number=%s, weight=%.2f\n", this.getModel(), this.getNumber(), this.getWeight());
+    }
+
+}
